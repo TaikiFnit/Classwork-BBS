f7339135c9f1345b9e97e4e4fbf53f5ecd3f589d
TaikiFnit <taikifnitspg@gmail.com>
2017-12-05 15:03:13 +0900

    create Models, Controllers

---
+++ app/Http/Controllers/BbsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Bbs;
+use Illuminate\Http\Request;
+
+class BbsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Bbs  $bbs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bbs $bbs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Bbs  $bbs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bbs $bbs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Bbs  $bbs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bbs $bbs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Bbs  $bbs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bbs $bbs)
+    {
+        //
+    }
+}
