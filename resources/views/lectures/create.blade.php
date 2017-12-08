f8641331a25752d4449ad6b50e3ad9b81181adfc
TaikiFnit <taikifnitspg@gmail.com>
2017-12-08 11:35:20 +0900

    #3 input fieldをrequired & 要素を大きく

--- resources/views/lectures/create.blade.php
+++ resources/views/lectures/create.blade.php
@@ -11,17 +11,18 @@ Create new Lecture
 
 <form action="{{ action('LectureController@store', ['school_id'=> $school_id]) }}" method="POST">
   <div class="form-group">
-    <label for="name">授業名 : </label><input type="text" name="name" id="name" placeholder="Name" class="form-control">
+    <label for="name">授業名 : </label><input type="text" name="name" id="name" placeholder="Name" class="form-control form-control-lg" required>
   </div>
   <div class="form-group">
-    <label for="teacher_name">担当教員名</label><input type="text" name="teacher_name" id="teacher_name" placeholder="担当教員名" class="form-control">
+    <label for="teacher_name">担当教員名</label><input type="text" name="teacher_name" id="teacher_name" placeholder="担当教員名" class="form-control form-control-lg" required>
   </div>
   <div class="form-group">
-    <label for="year">年 : </label><input type="number" name="year" id="year" placeholder="Year" class="form-control">
+    <label for="year">年 : </label><input type="number" name="year" id="year" placeholder="Year" class="form-control form-control-lg" required>
   </div>
     <input type="hidden" name="_token" value="{{csrf_token()}}">
-  <div class="form-group">
-    <input type="submit" value="Create" class="btn btn-primary">
+
+  <div class="form-group submit-group">
+    <input type="submit" value="Create" class="btn btn-primary btn-custom">
   </div>
 </form>
 
